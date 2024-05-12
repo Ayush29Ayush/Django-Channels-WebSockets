a5eb0b027de7b63e108a499c7613aab698b48d7f
Ayush Senapati <ayushsenapati123@gmail.com>
2024-05-12 21:29:37 +0530

    Consumers sending JSON data to Frontend

--- DjangoChannelsWebSocketApp/consumers.py
+++ DjangoChannelsWebSocketApp/consumers.py
@@ -1,8 +1,9 @@
-# Topic - Web API WebSocket - JavaScript
+# Topic - Real-time Data with Front End Example
 from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
 from time import sleep
 import asyncio
+import json 
 
 class MySyncConsumer(SyncConsumer):
   def websocket_connect(self, event):
@@ -11,13 +12,22 @@ class MySyncConsumer(SyncConsumer):
       'type':'websocket.accept',
     })
   
+  # def websocket_receive(self, event):
+  #   print('Message received from Client...', event['text'])
+  #   for i in range(10):
+  #     self.send({
+  #       'type':'websocket.send',
+  #       'text': str(i)
+  #     })
+  #     sleep(1)
+
+  #! Converting data in json format
   def websocket_receive(self, event):
-    print('Message received from Client', event)
-    print(event['text'])
+    print('Message received from Client...', event['text'])
     for i in range(10):
       self.send({
         'type':'websocket.send',
-        'text': str(i)
+        'text': json.dumps({"count":i})
       })
       sleep(1)
 
@@ -33,14 +43,22 @@ class MyAsyncConsumer(AsyncConsumer):
     })
   
   async def websocket_receive(self, event):
-    print('Message received from Client', event)
-    print(event['text'])
-    for i in range(50):
+    print('Message received from Client', event['text'])
+    for i in range(10):
       await self.send({
         'type':'websocket.send',
         'text': str(i)
       })
       await asyncio.sleep(1)
+
+  # async def websocket_receive(self, event):
+  #   print('Message received from Client', event['text'])
+  #   for i in range(10):
+  #     await self.send({
+  #       'type':'websocket.send',
+  #       'text': json.dumps({"count":i})
+  #     })
+  #     await asyncio.sleep(1)
 
   async def websocket_disconnect(self, event):
     print('Websocket Disconnected...', event)
