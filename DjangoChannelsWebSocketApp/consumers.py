e5c04f6971053264fbd80dfa2b6694a2231a7032
Ayush Senapati <ayushsenapati123@gmail.com>
2024-05-12 13:15:36 +0530

    Added CONSUMERS for websocket

--- DjangoChannelsWebSocketApp/consumers.py
+++ DjangoChannelsWebSocketApp/consumers.py
@@ -1,30 +1,41 @@
-# Topic - Routing
+# Topic - More on Consumer and Routing
 from channels.consumer import SyncConsumer, AsyncConsumer
 from channels.exceptions import StopConsumer
 
 class MySyncConsumer(SyncConsumer):
-    def websocket_connect(self, event):
-        print("Websocket Connected...", event)
-        self.send({"type": "websocket.accept"})
+  def websocket_connect(self, event):
+    print('Websocket Connected...', event)
+    self.send({
+      'type':'websocket.accept',
+    })
+  
+  def websocket_receive(self, event):
+    print('Message received from Client => ', event)
+    print(event['text'])
+    self.send({
+      'type':'websocket.send',
+      'text':'Message Sent to Client...'
+    })
 
-    def websocket_receive(self, event):
-        print("Messaged Received...", event)
-        print("Messaged is ", event["text"])
-
-    def websocket_disconnect(self, event):
-        print("Websocket Disconnected...", event)
-        raise StopConsumer()
-
+  def websocket_disconnect(self, event):
+    print('Websocket Disconnected...', event)
+    raise StopConsumer()
 
 class MyAsyncConsumer(AsyncConsumer):
-    async def websocket_connect(self, event):
-        print("Websocket Connected...", event)
-        await self.send({"type": "websocket.accept"})
+  async def websocket_connect(self, event):
+    print('Websocket Connected...', event)
+    await self.send({
+      'type':'websocket.accept',
+    })
+  
+  async def websocket_receive(self, event):
+    print('Message received from Client', event)
+    print(event['text'])
+    await self.send({
+      'type':'websocket.send',
+      'text':'Message Sent to Client...'
+    })
 
-    async def websocket_receive(self, event):
-        print("Messaged Received...", event)
-        print("Messaged is ", event["text"])
-
-    async def websocket_disconnect(self, event):
-        print("Websocket Disconnected...", event)
-        raise StopConsumer()
+  async def websocket_disconnect(self, event):
+    print('Websocket Disconnected...', event)
+    raise StopConsumer()
